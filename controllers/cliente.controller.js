01681f50086491e30283af6f1722e7aa6e21a26f
gustavofp <gu.fpoletto@gmail.com>
2017-09-12 00:51:38 -0300

    Update cliente.controller.js

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -2,12 +2,7 @@ const Cliente = require('../models/cliente')
 
 
 const createCliente = (req, res) => {
-    
-    if(!validarEmail(req.body.cnpj) || !validarCNPJ(req.body.email)){
-        res.send('email/CNPJ inválido')
-    }
-    else{
-    
+
         Cliente.create({
                 cnpj: req.body.cnpj,
                 razao_social: req.body.razao_social,
@@ -31,7 +26,6 @@ const createCliente = (req, res) => {
                     });
                 }
             });
-    }
 }
 
 const updateCliente = (req, res) => {
@@ -90,64 +84,5 @@ const getCliente = (req, res) => {
         });
 }
 
-function validarEmail(email){
-    var excluir=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-    var checar=/@[w-]+./;
-    var checarFinal=/.[a-zA-Z]{2,3}$/;
-    if(((email.search(excluir) != -1)||(email.search(checar)) == -1)||(email.search(checarFinal) == -1)){return false;}
-    else {return true;}
-}
-
-function validarCNPJ(cnpj) {
- 
-    cnpj = cnpj.replace(/[^\d]+/g,'');
- 
-    if(cnpj == '') return false;
-     
-    if (cnpj.length != 14)
-        return false;
-
-    if (cnpj == "00000000000000" || 
-        cnpj == "11111111111111" || 
-        cnpj == "22222222222222" || 
-        cnpj == "33333333333333" || 
-        cnpj == "44444444444444" || 
-        cnpj == "55555555555555" || 
-        cnpj == "66666666666666" || 
-        cnpj == "77777777777777" || 
-        cnpj == "88888888888888" || 
-        cnpj == "99999999999999")
-        return false;
-         
-    tamanho = cnpj.length - 2
-    numeros = cnpj.substring(0,tamanho);
-    digitos = cnpj.substring(tamanho);
-    soma = 0;
-    pos = tamanho - 7;
-    for (i = tamanho; i >= 1; i--) {
-      soma += numeros.charAt(tamanho - i) * pos--;
-      if (pos < 2)
-            pos = 9;
-    }
-    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado != digitos.charAt(0))
-        return false;
-         
-    tamanho = tamanho + 1;
-    numeros = cnpj.substring(0,tamanho);
-    soma = 0;
-    pos = tamanho - 7;
-    for (i = tamanho; i >= 1; i--) {
-      soma += numeros.charAt(tamanho - i) * pos--;
-      if (pos < 2)
-            pos = 9;
-    }
-    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-    if (resultado != digitos.charAt(1))
-          return false;
-           
-    return true;
-    
-}
 
 module.exports = { createCliente, deleteCliente, getCliente, updateCliente }
